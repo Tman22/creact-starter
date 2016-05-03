6038814adb62d664dff2da1ade56e79f583c8c39
Taylor Moore <Tmoore2272@gmail.com>
2016-05-03 11:38:57 -0600

    Sets up ajax call

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -1,5 +1,12 @@
 var AllSkills = React.createClass({
+  getInitialState() {
+    return { skills: [] }
+  },
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  },
   render() {
+    console.log(this.state)
     return(
       <div>
         <h1> Skills </h1>
