5c075d687ee53d18987e02e835ef13aec7fe113b
Taylor Moore <Tmoore2272@gmail.com>
2016-05-04 10:05:26 -0600

    Adds ability to create skill

--- app/assets/javascripts/components/_new_skill.js.jsx
+++ app/assets/javascripts/components/_new_skill.js.jsx
@@ -1,8 +1,30 @@
 var NewSkills = React.createClass({
+
+  handleClick() {
+    var title = this.refs.title.value;
+    var details = this.refs.details.value;
+
+    $.ajax({
+      url: "/api/v1/skills",
+      type: "POST",
+      data: { skill: {name: title, details: details } },
+      success: (skill) => {
+        this.props.handleSubmit(skill)
+        this.refs.title.value = ''
+        this.refs.details.value = ''
+      },
+      error: (response) => {
+        console.log('nope');
+      }
+    });
+  },
+
   render() {
     return(
       <div>
-
+        <input ref="title" placeholder="Enter Title" />
+        <input ref="details" placeholder="Details of skill" />
+        <button onClick={this.handleClick}>Submit</button>
       </div>
     )
   }
